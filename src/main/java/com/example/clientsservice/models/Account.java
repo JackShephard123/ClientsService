d1464fd050d35ec3251c4a18de3863f27b7ca889
favor <nicolas.shepelenko@gmail.com>
2022-11-08 21:44:26 +0200

    relation Client Account

--- src/main/java/com/example/clientsservice/models/Account.java
+++ src/main/java/com/example/clientsservice/models/Account.java
@@ -1,4 +1,44 @@
 package com.example.clientsservice.models;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+//
+@Entity
+@Table(name = "accounts")
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "int default 0")
+    private int amount;
+    @ManyToMany(mappedBy = "accounts", fetch = FetchType.LAZY)
+    private Set<Client> clients;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Account account)) return false;
+        return Objects.equals(id, account.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", amount=" + amount +
+                '}';
+    }
 }
