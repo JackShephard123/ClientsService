0c9ff32a59f046d008ecba5894dbbc52aea88dd8
favor <nicolas.shepelenko@gmail.com>
2022-11-08 21:03:57 +0200

    relation Client Phone equals() toString()

--- src/main/java/com/example/clientsservice/models/Phone.java
+++ src/main/java/com/example/clientsservice/models/Phone.java
@@ -3,6 +3,7 @@ package com.example.clientsservice.models;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -21,4 +22,24 @@ public class Phone {
     @JoinColumn(name = "client_id", nullable = false,
     foreignKey = @ForeignKey(name = "FK_phones_clients"))
     private Client client;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Phone phone1)) return false;
+        return Objects.equals(id, phone1.id) && Objects.equals(phone, phone1.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, phone);
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "id=" + id +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
 }
